4b13838058c3f8a46195277c96bdc26b2169db32
Simon <sema92m@wp.pl>
2022-04-16 20:43:45 +0200

    lesson20

--- js/sript.js
+++ js/sript.js
@@ -1,43 +1,99 @@
+'use strict';
+            //intro
 // alert('Hello');
-
 // const result = confirm("Are you here?");
 // console.log(result);
-
 // const answer = +prompt("Вам есть 18?", '');
 // console.log(answer + 5);
-
 // const user = 18;
-
 // alert(`Hello ${user}`);
-
 // let incr = 10,
 //     decr = 10;
-
 // // incr++;
 // // decr--;
-
 // console.log(++incr);
 // console.log(decr--);
-
 // console.log(5%2);
+            //intro
+//--------------------------------------------------
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
 
-const questionLastFilm1 = prompt('Один из последних просмотренних фильмов?',''),
-	questionRateFilm1 = +prompt('На сколько оцените его?',''),
-	questionLastFilm2 = prompt('Один из последних просмотренних фильмов?',''),
-	questionRateFilm2 = +prompt('На сколько оцените его?','');
 
-	personalMovieDB.movies[questionLastFilm1] = questionRateFilm1;
-	personalMovieDB.movies[questionLastFilm2] = questionRateFilm2;
 
-	console.log(personalMovieDB);
+             // //lesson1
+// const numberOfFilms = prompt('Сколько фильмов вы посмотрели?','');
+// console.log(numberOfFilms);
+
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
+
+// const a = prompt('Last film?',''),
+// 		b = prompt('Rating?',''),
+// 		c = prompt('Last film?',''),
+// 		d = prompt('Rating?','');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+               // //lesson1
+
+
+//---------------------------------------
+
+
+
+              //lesson2 (nr20)
+// if (1) {
+// 	console.log('ok');
+// } else {
+// 	console.log('error');
+// }
+
+// const num = 101;
+
+// if (num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.log('Much');
+// } else {
+// 	console.log('Ok');
+// }
+
+// const num = 50;
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+// const num = 111;
+// switch (num) {
+// 	case 49:
+// 		console.log('Nok');
+// 		break;
+// 	case 100:
+// 		console.log('Nok');
+// 		break;
+// 	case 50:
+// 		console.log('OK');
+// 		break;
+// 	default:
+// 		console.log('not today');
+		
+// }
+
+
+
+              //lesson 2 (nr 20)
+
+
+
+
+//---------------------------------------
+
+
+              //lesson 3 
