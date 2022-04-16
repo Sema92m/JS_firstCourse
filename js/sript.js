da00f8bf074ec3680d09822103a64d1904151c94
Simon <sema92m@wp.pl>
2022-04-16 22:55:42 +0200

    lesson 22

--- js/sript.js
+++ js/sript.js
@@ -96,4 +96,64 @@
 //---------------------------------------
 
 
-              //lesson 3 
+              //lesson 21 
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log('ok');
+// } else {
+// 	console.log('Nok');
+// }
+
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+//т.к нету колы то левая часть *или* будет false.  В правой части fries === 3 и 
+// это true и nuggets(должно быть 1 или больше) 2шт,
+// поэтому nuggets тоже true).
+// Оператор && вернет первое ложное значение но если его нету то значение ПОСЛЕДНЕГО АРГУМЕНТА!. 
+// оператор || сравнивает false c ДВУМЯ nuggest и вернёт правду. Ответ: 2
+
+
+           //lesson 21 
+
+
+		   //---------------------------------------
+
+
+              //lesson 22 
+//    let hamburger;
+//    const fries = NaN;
+//    const cola = 0;
+//    const nuggets = 2;
+	
+	
+//    if (hamburger || cola || fries === 3 || nuggets) {
+// 	  console.log('Done!');
+//    } else {
+// 	console.log('Nok');
+//    }
+
+// let hamburger = 1;
+// const fries = NaN;
+// const cola = 1;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!');
+// } else {
+// 	console.log('Nok');
+// }
+
+
+					//lesson 22 
+
+
+
+//---------------------------------------
+
+
