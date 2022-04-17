5dbed709c93a97128fddb35cf34262558519a854
Simon <sema92m@wp.pl>
2022-04-17 18:20:35 +0200

    solved some exersice

--- js/sript.js
+++ js/sript.js
@@ -156,4 +156,104 @@
 
 //---------------------------------------
 
+// Напишите условие if для проверки, что переменная age находится в диапазоне между 14 и 90 включительно.
+// «Включительно» означает, что значение переменной age может быть равно 14 или 90.
 
+// const age = 91;
+
+// if ( age >= 14 && age <= 90) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+
+// Напишите условие if для проверки, что значение переменной age НЕ находится в диапазоне 14 и 90 включительно.
+
+// Напишите два варианта: первый с использованием оператора НЕ !, второй – без этого оператора.
+
+// const age = 91;
+
+// if (age >= 14 && age <= 90) {
+//     console.log('В диапазоне 14-90');
+// } else {
+//     console.log('Вне диапазона 14-90');
+// // }
+
+// const age = 55;
+
+// if (age < 14 || age > 90) {
+//     console.log('Вне диапазона 14-90');
+// } else {
+//     console.log('В диапазоне 14-90');
+// }
+
+
+// if (-1 || 0) alert( 'first' );   //-1 это true, alert will work
+// if (-1 && 0) alert( 'second' );  //0=false, won't work
+// if (null || -1 && 1) alert( 'third' );//null|| -1   ->  -1=true , alert will work
+
+
+
+
+
+// Проверка логина
+// важность: 3
+// Напишите код, который будет спрашивать логин с помощью prompt.
+
+// Если посетитель вводит «Админ», то prompt запрашивает пароль, если ничего не введено или нажата клавиша Esc –
+// показать «Отменено», в противном случае отобразить «Я вас не знаю».
+
+// Пароль проверять так:
+
+// Если введён пароль «Я главный», то выводить «Здравствуйте!»,
+// Иначе – «Неверный пароль»,
+// При отмене – «Отменено».
+// Блок-схема:
+
+
+// let login = prompt('who are you?', ['Admin']);
+
+// if (login === 'Admin') { 
+
+//     let pass = prompt('Password','');
+
+//     if (pass === '1111') {
+//         alert('Hello');
+//     } else if (pass == '') {
+//         alert('Cancel');
+//     } else {
+//         alert('Incorrect password');
+//     }
+
+// } else if (login == null || login == '') {
+//     alert('Cancel');
+// } else {
+//     alert('I dont know who you are');
+// }
+
+
+
+//перешал сам 2й раз, добавил поведение при вводе пустой строки вместо пароля
+
+let login = prompt('who are you?', ['Admin']);
+
+if (login === 'Admin') {
+
+    let password = prompt('Password?',''); 
+
+        if (password === '1111') {
+        alert('Hello');
+    } else if (password == null) {
+        alert('Cancel');
+    } else if (password == '') {
+        alert('Incorrect pasword');
+    } else {
+        alert('Incorrect pasword');
+    }
+} else if (login == null || login == '') {
+    alert('Cancel');
+} else {
+    alert('I dont know who are you');
+}
