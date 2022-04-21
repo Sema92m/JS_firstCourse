b434027130efd427a9491d291eeee0c3c041bb59
Simon <sema92m@wp.pl>
2022-04-21 20:33:19 +0200

    add coding exercise nr7

--- js/script.js
+++ js/script.js
@@ -711,7 +711,30 @@
 //     }
 
 //     return str;
-// }
+// } 
 
 
 // console.log(getMathResult(5,4));
+
+
+
+// const str = "teSt";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf('fruit'));
+
+
+// const logg = "Hello world";
+// // console.log(logg.slice(6, 11));
+// // console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+// const num = 12.2;
+// console.log(Math.round(num));
+
+
+// const test = "12.2px";
+// console.log(parseInt(test));//переводит число в дургую систему  измерения(из строки в число)
+// console.log(parseFloat(test));//возращает в десятичном варианте, возващает с точкой
