a4feaf1a2b9e66db57988b1b9b41986adf4853e4
Simon <sema92m@wp.pl>
2022-05-17 22:34:23 +0200

    coding exercise 15 done

--- js/theory.js
+++ js/theory.js
@@ -221,3 +221,71 @@ console.log('Учим JS');
 // console.log("ёжик" > "Ёжик");
 
 // console.log(0 || "" || 2 || undefined || true || false);
+
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true,
+};
+
+
+
+function isOpen(prop) {
+    let answer = '';
+    prop == false ? answer = 'Закрыто' : answer = 'Открыто';
+
+    return answer;
+}
+console.log(isOpen(restorantData.openNow));
+
+
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if ((parseFloat(fDish.price) + parseFloat(sDish.price)) < parseFloat(average)) {
+        return 'Цена ниже средней';
+    }  if ((parseFloat(fDish.price) + parseFloat(sDish.price)) == parseFloat(average)) {
+        return 'Цена средняя';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+
+
+
+
+
+function transferWaitors(data) {
+    const copy = JSON.parse(JSON.stringify(data));
+	copy.waitors[0] = ({name: 'Mike', age: 32});
+    return copy;
+}
+
+transferWaitors(restorantData);
+
+
+console.log(transferWaitors(restorantData));
+console.log(restorantData.waitors);
+
