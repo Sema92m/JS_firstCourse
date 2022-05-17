3acb615d3dd3e664c504c68be3443b17c4e17853
Simon <sema92m@wp.pl>
2022-05-17 19:28:25 +0200

    add coding ex lesson  41

--- js/theory.js
+++ js/theory.js
@@ -193,5 +193,31 @@ console.log('Учим JS');
 // строгий режим НЕ активирован
 */
 
+// [] + false - null + true
+// console.log([] + false - null + true);
 
+// let y = 1;
+// let x = y = 2;
+// alert(x);
 
+// console.log([] + 1 + 2);
+// console.log(typeof([] + 1 + 2));
+
+// alert( "123r5u"[3] );
+
+// 2 && 1 && null && 0 && undefined
+// console.log(2 && 1 && null && 0 && undefined);
+
+// console.log(!!( 1 && 2 ) === (1 && 2));
+
+// alert( null || 2 && 3 || 4 );
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// console.log(!!(a == b));
+
+// alert( +"Infinity" );
+
+// console.log("ёжик" > "Ёжик");
+
+// console.log(0 || "" || 2 || undefined || true || false);
