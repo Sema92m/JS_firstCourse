1764bf40e36100f3ce2fe50923df18279ae25ece
Simon <sema92m@wp.pl>
2023-04-08 09:31:50 +0200

    feat: shelter-week3 slider works, but no animation & adaptive

--- js/codewars.js
+++ js/codewars.js
@@ -5589,3 +5589,14 @@ function getParticipants3212(h) {
   }
   return res;
 }
+function getParticipants32312(h) {
+  if (h === 0) return 0;
+  if (h === 1) return 2;
+  let res = 0;
+  let sum = 0;
+  while (h > sum) {
+      sum = (res * (res + 1)) / 2;
+      ++res;
+  }
+  return res;
+}
