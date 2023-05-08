5724e294f8658d68e767e43308d019e6eb3ace4f
Simon <sema92m@wp.pl>
2023-05-08 21:08:33 +0200

    feat: finished 5/6 tasks HTML Builder

--- js/stars-tree.js
+++ js/stars-tree.js
@@ -9,7 +9,7 @@
 //         break
 //     }
 //   console.log('*'.repeat(i));
-}//обратная половина елки(от 5 к 1)
+//обратная половина елки(от 5 к 1)
 
 // let lines = (line = 5),
 //     a = ".",
@@ -21,22 +21,20 @@
 //             Array(line + 1).join(a)
 //     );
 
-
-
 function* generateFirTree(height) {
-	let blanks = ' '.repeat(height - 1), // (1)
-	asterisks = "*"                      // (2)
-	
-	for (let i = 0; i < height; i++) {
-	  yield blanks + asterisks + blanks  // (3)
-	  blanks = blanks.slice(0, -1)       // (4)
-	  asterisks += "**"                  // (5)
-	}                                    // (6)
-  }
-  
-  for (let line of generateFirTree(34)) {
-	console.log(line)
-  }
+    let blanks = " ".repeat(height - 1), // (1)
+        asterisks = "*"; // (2)
+
+    for (let i = 0; i < height; i++) {
+        yield blanks + asterisks + blanks; // (3)
+        blanks = blanks.slice(0, -1); // (4)
+        asterisks += "**"; // (5)
+    } // (6)
+}
+
+for (let line of generateFirTree(34)) {
+    console.log(line);
+}
 //     Как это работает generateFirTree:
 
 //1 Инициализируем начальное количество пробелов равное высота дерева - 1
