b61b62fbc653676bfea5edd52c907e9193e4b089
Semi John <sema92m@wp.pl>
2024-03-19 17:17:55 +0100

    feat: 6ku katas added

--- js/codewars.js
+++ js/codewars.js
@@ -3420,3 +3420,115 @@ multiplicationTable = function (size) {
     return res;
 };
 console.log(multiplicationTable(4));
+
+var whatTimeIsIt = function (angle) {
+    let hour = (angle - (angle % 30)) / 30;
+    let min = Math.floor((angle % 30) * 2);
+    if (hour > 0 && hour < 10) {
+        hour = "0" + hour;
+    }
+    if (hour === 0) {
+        hour = 12;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+
+    return `${hour}:${min}`;
+};
+console.log(whatTimeIsIt(17.888631909538777));
+
+var list1 = [
+    {
+        firstName: "Harry",
+        lastName: "K.",
+        country: "Brazil",
+        continent: "Americas",
+        age: 19,
+        language: "Python",
+    },
+    {
+        firstName: "Kseniya",
+        lastName: "T.",
+        country: "Belarus",
+        continent: "Europe",
+        age: 29,
+        language: "JavaScript",
+    },
+    {
+        firstName: "Jing",
+        lastName: "X.",
+        country: "China",
+        continent: "Asia",
+        age: 39,
+        language: "Ruby",
+    },
+    {
+        firstName: "Noa",
+        lastName: "A.",
+        country: "Israel",
+        continent: "Asia",
+        age: 40,
+        language: "Ruby",
+    },
+    {
+        firstName: "Andrei",
+        lastName: "E.",
+        country: "Romania",
+        continent: "Europe",
+        age: 59,
+        language: "C",
+    },
+    {
+        firstName: "Maria",
+        lastName: "S.",
+        country: "Peru",
+        continent: "Americas",
+        age: 60,
+        language: "C",
+    },
+    {
+        firstName: "Lukas",
+        lastName: "X.",
+        country: "Croatia",
+        continent: "Europe",
+        age: 75,
+        language: "Python",
+    },
+    {
+        firstName: "Chloe",
+        lastName: "K.",
+        country: "Guernsey",
+        continent: "Europe",
+        age: 88,
+        language: "Ruby",
+    },
+    {
+        firstName: "Viktoria",
+        lastName: "W.",
+        country: "Bulgaria",
+        continent: "Europe",
+        age: 98,
+        language: "PHP",
+    },
+    {
+        firstName: "Piotr",
+        lastName: "B.",
+        country: "Poland",
+        continent: "Europe",
+        age: 128,
+        language: "JavaScript",
+    },
+];
+function isAgeDiverse(list) {
+    if (list.length < 10) {
+        return false;
+    }
+    let ages = [];
+    for (let i = 0; i < list.length; i++) {
+        ages.push(list[i].age);
+    }
+
+    return ages;
+}
+console.log(isAgeDiverse(list1));
