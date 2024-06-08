b45beb1e47f832546f1298b6808ba183065a6637
Semi John <sema92m@wp.pl>
2024-06-08 19:46:59 +0200

    5ku kata add

--- js/codewars.js
+++ js/codewars.js
@@ -3675,3 +3675,55 @@ function orderWeight(strng) {
     return arr.join(" ");
 }
 console.log(orderWeight("112 32 6 33 42 7"));
+
+Math.round = function (number) {
+    if (!String(number).split("").includes(".")) {
+        return number;
+    }
+    const remainder = String(number).match(/\.(\d+)$/)[1];
+    const base = +String(number).match(/^(\d+)\./)[1];
+    if (remainder && +remainder[0] >= 5) {
+        return base + 1;
+    }
+    return base;
+};
+
+Math.ceil = function (number) {
+    if (number == 0) {
+        return 0;
+    }
+    const num = String(number).split("").includes(".");
+    if (!num) {
+        return number;
+    } else {
+        return +String(number).match(/^(\d+)\./)[1] + 1;
+    }
+};
+
+Math.floor = function (number) {
+    const num = String(number).split("").includes(".");
+
+    if (!num && number !== 0) {
+        return number;
+    }
+    if (number == 0) {
+        return 0;
+    }
+    const base = +String(number).match(/^(\d+)\./)[1];
+    if (num) {
+        return base;
+    } else if (num) {
+        return 0;
+    }
+};
+const num = 5;
+console.log([
+    "round " + Math.round(num),
+    "ceil " + Math.ceil(num),
+    "floor " + Math.floor(num),
+]);
+console.log(parseInt(1.99));
+
+// Math.floor = number => parseInt(number)
+// Math.round = number => Math.floor(number + 0.5)
+// Math.ceil  = number => Number.isInteger(number) ? number : Math.floor( number + 1 )
--- js/functions/codewarsSinglTask.js
+++ js/functions/codewarsSinglTask.js
@@ -1,30 +1,51 @@
-function orderWeight(strng) {
-    let str = strng.replace(/\s+/g, " ").trim().split(" ");
-    if (str.join("") === "") {
-        return "";
+Math.round = function (number) {
+    if (!String(number).split("").includes(".")) {
+        return number;
     }
-    const arr = str
-        .map((el) =>
-            el
-                .split("")
-                .map((el) => +el)
-                .reduce((a, b) => a + b)
-        )
-        .sort((a, b) => a - b);
-    const sortArr = str.sort();
-    // console.log(["sortArr", sortArr]);
-    // console.log(["arr", arr]);
-    //
-    for (let i = 0; i < sortArr.length; i++) {
-        let num = +[sortArr[i]].map((el) =>
-            el.split("").reduce((a, b) => +a + +b)
-        )[0];
-        // console.log(typeof num);
-        if (arr.includes(num)) {
-            arr[arr.indexOf(num)] = sortArr[i];
-        }
+    const remainder = String(number).match(/\.(\d+)$/)[1];
+    const base = +String(number).match(/^(\d+)\./)[1];
+    if (remainder && +remainder[0] >= 5) {
+        return base + 1;
     }
-    // console.log(sortArr);
-    return arr.join(" ");
-}
-console.log(orderWeight("112 32 6 33 42 7"));
+    return base;
+};
+
+Math.ceil = function (number) {
+    if (number == 0) {
+        return 0;
+    }
+    const num = String(number).split("").includes(".");
+    if (!num) {
+        return number;
+    } else {
+        return +String(number).match(/^(\d+)\./)[1] + 1;
+    }
+};
+
+Math.floor = function (number) {
+    const num = String(number).split("").includes(".");
+
+    if (!num && number !== 0) {
+        return number;
+    }
+    if (number == 0) {
+        return 0;
+    }
+    const base = +String(number).match(/^(\d+)\./)[1];
+    if (num) {
+        return base;
+    } else if (num) {
+        return 0;
+    }
+};
+const num = 5;
+console.log([
+    "round " + Math.round(num),
+    "ceil " + Math.ceil(num),
+    "floor " + Math.floor(num),
+]);
+console.log(parseInt(1.99));
+
+// Math.floor = number => parseInt(number)
+// Math.round = number => Math.floor(number + 0.5)
+// Math.ceil  = number => Number.isInteger(number) ? number : Math.floor( number + 1 )
