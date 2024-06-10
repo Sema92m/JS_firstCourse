1c126969bf4d8944ab16bed06878fe149c095a9b
Semi John <sema92m@wp.pl>
2024-06-10 22:20:03 +0200

    5ku kata add

--- js/functions/codewarsSinglTask.js
+++ js/functions/codewarsSinglTask.js
@@ -1,40 +1,27 @@
-function createSpiral(N) {
-    console.log(!Number.isInteger("N"));
-    if (!Number.isInteger(N) || N < 1 || typeof N !== "number") {
-        return [];
+function incrementString(strng) {
+    if (
+        strng.replace(/[\D]/gi, "") === strng &&
+        String(Number(strng)) === strng
+    ) {
+        return String(Number(strng) + 1);
     }
-    let result = new Array(N).fill().map(() => new Array(N).fill(""));
-    let counter = 1;
-    let fc = 0;
-    let lc = N - 1;
-    let fr = 0;
-    let lr = N - 1;
+    let remNums = "";
+    let mainPart = "";
+    for (let i = strng.length - 1; i > 0; i--) {
+        if (Number(strng[i]) || strng[i] === "0") {
+            remNums = remNums + strng[i];
+        } else {
+            break;
+        }
+    }
+    remNums = remNums.split("").reverse().join("");
+    mainPart = strng.slice(0, strng.length - remNums.length);
 
-    while (fc <= lc && fr <= lr) {
-        for (let i = fc; i <= lc; i++) {
-            result[fr][i] = counter;
-            counter++;
-        }
-        fr++;
-        for (let i = fr; i <= lr; i++) {
-            result[i][lc] = counter;
-            counter++;
-        }
-        lc--;
-        for (let i = lc; i >= fc; i--) {
-            result[lr][i] = counter;
-            counter++;
-        }
-        lr--;
-        for (let i = lr; i >= fr; i--) {
-            result[i][fc] = counter;
-            counter++;
-        }
-        fc++;
+    if (remNums[0] === "0") {
+        remNums = String(+("10" + remNums) + 1).slice(2);
+        return mainPart + remNums;
     }
-    return result;
+    return mainPart + (Number(remNums) + 1);
 }
-// console.log(createSpiral(1));
-// console.log(createSpiral(3));
-// console.log(createSpiral(0));
-console.log(createSpiral(5.5));
+
+console.log(incrementString("1"));
